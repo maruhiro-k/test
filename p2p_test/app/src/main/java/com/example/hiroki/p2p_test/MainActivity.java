9a2caea11ffc98af5963997fe9705e6cb31d4b6a
maruhiro-k <maru_kanzou@hotmail.com>
2016-01-09 15:39:29 +0900

    P2Pのテストプロジェクト追加

---
+++ p2p_test/app/src/main/java/com/example/hiroki/p2p_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hiroki.p2p_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
