c13bd580ef74c380749360f2baec8ea444109677
maruhiro-k <maru_kanzou@hotmail.com>
2016-01-11 18:47:34 +0900

    P2P確認用メソッドを書いてみた

---
+++ p2p_test/app/src/main/java/com/example/hiroki/p2p_test/p2p/Connector.java
@@ -0,0 +1,11 @@
+package com.example.hiroki.p2p_test.p2p;
+
+import android.content.Context;
+
+/**
+ * Created by hiroki on 2016/01/11.
+ */
+public interface Connector {
+
+    public boolean isEnabled();
+}
