a2bd0d31b04a3fe9a7271fa0388051c6eab4ca7a
maruhiro-k <maru_kanzou@hotmail.com>
2016-01-23 15:21:37 +0900

    ソース整理

--- p2p_test/app/src/main/java/com/example/hiroki/p2p_test/HomeActivity.java
+++ p2p_test/app/src/main/java/com/example/hiroki/p2p_test/HomeActivity.java
@@ -1,78 +1,43 @@
 package com.example.hiroki.p2p_test;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hiroki.p2p_test.p2p.WiFiDirectBroadcastReceiver;
-import com.example.hiroki.p2p_test.p2p.WifiDirectConnector;
 
 public class HomeActivity extends AppCompatActivity {
-    WifiDirectConnector cnct;
-    WifiP2pManager mManager;
-    WifiP2pManager.Channel mChannel;
-    BroadcastReceiver mReceiver;
-    IntentFilter mIntentFilter;
+    WiFiDirectBroadcastReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        cnct = new WifiDirectConnector(getApplicationContext());
-
-        if (!cnct.isEnabled()) {
-            Toast.makeText(this, "not psp!", Toast.LENGTH_LONG).show();
-        }
-        else {
-//            Toast.makeText(this, "ok psp!", Toast.LENGTH_LONG).show();
-        }
-
-        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
-        mChannel = mManager.initialize(getApplicationContext(), getMainLooper(), null);
-        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
-        mIntentFilter = new IntentFilter();
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
-        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
-
-        Log.d("mManager", mManager.toString());
-        Log.d("mChannel", mChannel.toString());
-        Log.d("mReceiver", mReceiver.toString());
-        Log.d("mIntentFilter", mIntentFilter.toString());
-
         Button btn = (Button) findViewById(R.id.button2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(HomeActivity.this, BattleActivity.class);
-//                startActivity(intent);
-                mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+                mReceiver = new WiFiDirectBroadcastReceiver(HomeActivity.this, new WiFiDirectBroadcastReceiver.LogAction() {
+                    int num = 0;
                     @Override
-                    public void onSuccess() {
-                        Log.d("discover", "success");
-                    }
-
-                    @Override
-                    public void onFailure(int reasonCode) {
-                        Toast.makeText(getApplicationContext(), "fail: " + String.format("%d", reasonCode), Toast.LENGTH_LONG).show();
+                    public void add(String log) {
+                        Log.d("HomeActivity", log);
+                        TextView t = (TextView) HomeActivity.this.findViewById(R.id.textView3);
+                        t.setMovementMethod(ScrollingMovementMethod.getInstance());
+                        t.append(++num + ":" + log + "\n");
                     }
                 });
+
+                // 検索
+                mReceiver.search();
+                // 接続
+                // 適当に通信してみる
             }
         });
     }
@@ -87,7 +52,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.d("pause", "pause2");
-        unregisterReceiver(mReceiver);
+
+        if (mReceiver != null) {
+            mReceiver.close();
+            mReceiver = null;
+        }
     }
 
     @Override
@@ -106,7 +75,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d("resume", "resume2");
-        registerReceiver(mReceiver, mIntentFilter);
     }
 
     @Override
--- p2p_test/app/src/main/java/com/example/hiroki/p2p_test/p2p/WiFiDirectBroadcastReceiver.java
+++ p2p_test/app/src/main/java/com/example/hiroki/p2p_test/p2p/WiFiDirectBroadcastReceiver.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.p2p.WifiP2pConfig;
 import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pDeviceList;
@@ -14,15 +16,67 @@ import android.widget.Toast;
 import java.util.Collection;
 
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
-    WifiP2pManager manager;
-    WifiP2pManager.Channel channel;
-    Activity activity;
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    IntentFilter mIntentFilter;
+    Context mContext;
+    LogAction mLogger;
 
-    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, Activity activity) {
+
+    public interface LogAction {
+        public abstract void add(String log);
+    }
+
+    public WiFiDirectBroadcastReceiver(Context c, LogAction logger) {
         super();
-        this.manager = manager;
-        this.channel = channel;
-        this.activity = activity;
+
+        mContext = c;
+        mManager = (WifiP2pManager) c.getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(c, c.getMainLooper(), null);
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mLogger = logger;
+        mLogger.add("start");
+        start();
+    }
+
+    public boolean isEnabled() {
+        return mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_DIRECT);
+    }
+
+    public boolean start() {
+        if (!isEnabled()) {
+            return false;
+        }
+        mContext.registerReceiver(this, mIntentFilter);
+        return true;
+    }
+
+    public void close() {
+        mContext.unregisterReceiver(this);
+    }
+
+    public void search() {
+        if (!isEnabled()) {
+            mLogger.add("P2P Disabled!");
+            return;
+        }
+
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                mLogger.add("discover: success");
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                mLogger.add("discover: fail: " + String.format("%d", reasonCode));
+            }
+        });
     }
 
     @Override
@@ -33,42 +87,59 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
                 // Wifi P2P is enabled
-                Log.d("check", "P2P enabled!");
+                mLogger.add("check: P2P enabled!");
             } else {
                 // Wi-Fi P2P is not enabled
-                Toast.makeText(context, "Error: p2p disabled!", Toast.LENGTH_LONG).show();
+                mLogger.add("Error: p2p disabled!");
             }
 
         }
         else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             // Call WifiP2pManager.requestPeers() to get a list of current peers
-            if (manager != null) {
-                manager.requestPeers(channel, new WifiP2pManager.PeerListListener() {
+            if (mManager != null) {
+                mManager.requestPeers(mChannel, new WifiP2pManager.PeerListListener() {
                     @Override
                     public void onPeersAvailable(WifiP2pDeviceList peers) {
-                        Log.d("found peers", "test" + peers.toString());
-                        Toast.makeText(activity,  peers.toString(), Toast.LENGTH_LONG).show();
-
                         Collection<WifiP2pDevice> decices = peers.getDeviceList();
+                        if (decices.isEmpty()) {
+                            mLogger.add("not found peers: " + peers.toString());
+                            return;
+                        }
+                        mLogger.add("found peers: " + peers.toString());
+
                         WifiP2pConfig config = new WifiP2pConfig();
                         config.deviceAddress = decices.iterator().next().deviceAddress;
-                        manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+                        mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
 
                             @Override
                             public void onSuccess() {
                                 //success logic
-                                Log.d("connect", "suc");
+                                mLogger.add("connect success");
                             }
 
                             @Override
                             public void onFailure(int reason) {
                                 //failure logic
-                                Log.d("connect", "fail: " + String.format("%d", reason));
+                                mLogger.add("connect: fail: " + String.format("%d", reason));
                             }
                         });
 
                     }
                 });
+
+                /*
+                mManager.stopPeerDiscovery(mChannel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        mLogger.add("stop success");
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        mLogger.add("stop: fail: " + String.format("%d", reason));
+                    }
+                });
+                */
             }
         }
         else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
